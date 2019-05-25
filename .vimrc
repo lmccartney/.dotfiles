9196dd62e9ab473a30fd187e7c8500b9dc5193f8
Lennon McCartney <mccartney.photo@gmail.com>
2019-05-25 16:56:49 -0400

    new things or something

--- .vimrc
+++ .vimrc
@@ -1,2 +1,7 @@
 syntax on
 filetype indent plugin on
+colorscheme badwolf
+set listchars=tab:>=,trail:.
+set list
+highlight OverLength ctermbg=red ctermfg=white
+match OverLength /\%81v.\+/
--- setup.sh
+++ setup.sh
@@ -4,15 +4,15 @@ mkdir -p ${HOME}/.dotbackups
 
 mv ${HOME}/.vimrc ${HOME}/.dotbackups/
 cp ${HOME}/.bashrc ${HOME}/.dotbackups/
-cp .vimrc ${HOME}/
-cp .tmux.conf ${HOME}/
+cp ${HOME}/.gitrepos/.dotfiles/.vimrc ${HOME}/
+cp ${HOME}/.gitrepos/.dotfiles/.tmux.conf ${HOME}/
 
 # start badwolf setup
 if [ ! -d "${HOME}/.gitrepos/badwolf" ]; then
 	git clone git@github.com:sjl/badwolf.git ${HOME}/.gitrepos/badwolf/
 else
 	cd ${HOME}/.gitrepos/badwolf/
-	git pull
+	git pull &> /dev/null
 fi
 
 if [ ! -f "${HOME}/.vim/colors/badwolf.vim" ]; then
@@ -26,11 +26,23 @@ if [ ! -z $( grep -Fxq "export TERM=xterm-256color" ${HOME}/.bashrc ) ]; then
 fi
 # end badwolf setup
 
+# start syntastic setup
+if [ ! -d "${HOME}/.gitrepos/vim-syntastic" ]; then
+	git clone git@github.com:vim-syntastic/syntastic.git ${HOME}/.gitrepos/vim-syntastic/
+else
+	cd ${HOME}/.gitrepos/vim-syntastic/
+	git pull &> /dev/null
+fi
+
+cp -rf ${HOME}/.gitrepos/vim-syntastic/autoload ${HOME}/.vim/
+cp -rf ${HOME}/.gitrepos/vim-syntastic/doc ${HOME}/.vim/
+cp -rf ${HOME}/.gitrepos/vim-syntastic/plugin ${HOME}/.vim/
+cp -rf ${HOME}/.gitrepos/vim-syntastic/syntax_checkers ${HOME}/.vim/
+
 # start pyenv setup
 if [ ! -d "${HOME}/.pyenv" ]; then
 	curl https://pyenv.run | bash
 	if [ ! -z $( grep -Fxq "export PATH=\"${HOME}/.pyenv/bin:\$PATH" ${HOME}/.bashrc ) ];  then
-		echo "not found"
 		echo "export PATH=${HOME}/.pyenv/bin:\$PATH" >> ${HOME}/.bashrc
 		echo "eval \"\$(pyenv init -)\"" >> ${HOME}/.bashrc
 		echo "eval \"\$(pyenv virtualenv-init -)\"" >> ${HOME}/.bashrc
